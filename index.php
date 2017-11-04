ae410533aa085938e8fb431e856de6664d336313
erjosito <erjosito@hotmail.com>
2017-11-04 12:13:11 +0100

    daemon test

--- index.php
+++ index.php
@@ -9,10 +9,12 @@
      <br>
      <?php
          $reachabilityTest = False;
+         $daemonTest = True;
      ?>
      <?php
-        # Reachability test
-        if (reachabilityTest) {
+        // Reachability test
+        if ($reachabilityTest) {
+            print ("<h2>Reachability Test</h2>");
             $hosts = array ("bing.com", "google.com");
             $allReachable = true;
             foreach ($hosts as $host) {
@@ -26,7 +28,7 @@
             if ($allReachable === false) {
               // Ping did not work
               http_response_code (299);
-              print ("At least one target host does not seem to be all reachable (" . $host . ")\n");
+              print ("At least one target host does not seem to be reachable (" . $host . ")\n");
             } else {
               // Ping did work
               http_response_code (200);
@@ -34,5 +36,32 @@
             }
         }
      ?>
+
+     <?php
+        // Daemon test
+        if ($daemonTest) {
+            print ("<h2>Daemon Test</h2>");
+            $daemons = array ("httpd", "sshd");
+            $allRunning = true;
+            foreach ($daemon as $daemons) {
+              $result = exec ("systemctl status " . $daemon . " 2>&1 | grep 'active (running)'");
+              $pos = strpos ($result, "running");
+              if ($pos === false) {
+                $allRunning = false;
+                break;
+              }
+            }
+            if ($allRunning === false) {
+              // Daemon not running
+              http_response_code (298);
+              print ("At least one daemon does not seem to be running (" . $daemon . ")\n");
+            } else {
+              // All daemons running
+              http_response_code (200);
+              print ("All daemons seem to be running\n");
+            }
+        }
+     ?>
+
    </body>
 </html>
